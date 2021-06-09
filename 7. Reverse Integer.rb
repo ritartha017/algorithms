e41ae7b116df89c84990dbe3052e541a17fe38c8
ritartha017 <tsmi017@bk.ru>
2021-06-09 21:22:15 +0300

    added probl 7

---
+++ 7. Reverse Integer.rb
@@ -0,0 +1,20 @@
+# Given a signed 32-bit integer x, return x with its digits reversed. 
+# If reversing x causes the value to go outside 
+# the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+#Example 1:
+# Input: x = 123
+# Output: 321
+
+# Example 2:
+#Input: x = -123
+#Output: -321
+
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+    ans = x.to_s.reverse!.to_i
+    ans = x < 0 ? -ans : ans
+    return 0 if ans < -(2**31) || ans > (2**31)-1
+    ans
+end
