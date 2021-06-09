4fa6df85a2f740d42bfe0182c84690ec56fa0977
ritartha017 <tsmi017@bk.ru>
2021-06-09 21:24:35 +0300

    added probl 7

--- 7. Reverse Integer.rb
+++ 7. Reverse Integer.rb
@@ -2,13 +2,13 @@
 # If reversing x causes the value to go outside 
 # the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
-#Example 1:
+# Example 1:
 # Input: x = 123
 # Output: 321
 
 # Example 2:
-#Input: x = -123
-#Output: -321
+# Input: x = -123
+# Output: -321
 
 # @param {Integer} x
 # @return {Integer}
