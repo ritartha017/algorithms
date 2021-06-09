3dc116cd246bcfa41095f80b889ce511fd2b7209
ritartha017 <tsmi017@bk.ru>
2021-06-09 13:12:57 +0300

    added probl no1

---
+++ 1. Two Sum.rb
@@ -0,0 +1,38 @@
+
+#Brute Force Method - Time O(n^2), Space O(1)
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    (0..nums.length - 1).each do |i|
+        j = i + 1
+        (j..nums.length - 1).each do |j|
+            return [i, j] if nums[i] + nums[j] == target
+        end
+    end
+end  
+
+=begin
+#The Hash Table Solution - Time O(n), Space O(1)
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @param {Integer{}} hash
+# @return {Integer[]}
+def two_sum(nums, target, hash = {})
+    nums.each_with_index do |val, i|
+    return [hash[target-val], i] if hash.has_key?(target-val)
+    
+    hash[val] = i
+    end
+end
+=end
+
+out = two_sum([3, 2, 4], 6)
+
+out.each do |x|
+    puts "#{x} "
+end  
+
+gets
